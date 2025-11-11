a0194dcf69a1112fd8571c9f3ce4922f9abd6a8b
Daniel Eric Antmann <tula3x3@gmail.com>
2025-11-11 14:36:18 +0100

    feat(app):  install dotenv, create env file, update gitignore and data-source for future migrations

--- backend/infrastructure/orm/data-source.ts
+++ backend/infrastructure/orm/data-source.ts
@@ -1,18 +1,19 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
 
-// import { Pet } from '../../core/pets/domain/Pet';
-// import { Owner } from '../../core/owners/domain/Owner';
-// import { Appointment } from '../../core/appointments/domain/Appointment';
-// import { Service } from '../../core/services/domain/Service';
+import { Pet } from '../../core/pets/domain/Pet';
+import { Owner } from '../../core/owners/domain/Owner';
+import { Appointment } from '../../core/appointments/domain/Appointment';
+import { Service } from '../../core/services/domian/Service';
 import { Breed } from '../../core/breeds/domain/Breed';
 import { Animal } from '../../core/animals/domain/Animal';
 
+dotenv.config();
 export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'db.sqlite',
-  synchronize: true,
-  logging: false,
-  //   entities: [Pet, Owner, Appointment, Service, Breed, Animal],
-  entities: [Animal, Breed],
+  type: process.env.DB_TYPE as any,
+  database: process.env.DB_PATH,
+  synchronize: process.env.TYPEORM_SYNC === 'true',
+  logging: process.env.TYPEORM_LOGGING === 'true',
+  entities: [Pet, Owner, Appointment, Service, Breed, Animal],
 });
