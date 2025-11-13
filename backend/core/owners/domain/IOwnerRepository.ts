bc9dd2f5fc7fab5a39ae664f768675eebdd36403
Daniel Eric Antmann <tula3x3@gmail.com>
2025-11-13 23:03:20 +0100

    feature(owner):  create IOwnerRepository

---
+++ backend/core/owners/domain/IOwnerRepository.ts
@@ -0,0 +1,11 @@
+import { Owner } from './Owner';
+
+export interface IOwnerRepository {
+  create(owner: Owner): Promise<Owner>;
+  update(id: number, owner: Owner): Promise<Owner | null>;
+  delete(id: number): Promise<boolean>;
+  findAll(): Promise<Owner[]>;
+  findById(id: number): Promise<Owner | null>;
+  findByName(name: string): Promise<Owner | null>;
+  findByEmail(email: string): Promise<Owner | null>;
+}
